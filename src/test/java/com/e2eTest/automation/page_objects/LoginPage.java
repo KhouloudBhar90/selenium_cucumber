05b4757dbd9ca9434b7ed660e623da458f60764c
Khouloud Bhar <khouloud.bhar@neoxam.com>
2023-02-18 00:20:51 +0100

    Update Project

--- src/test/java/com/e2eTest/automation/page_objects/LoginPage.java
+++ src/test/java/com/e2eTest/automation/page_objects/LoginPage.java
@@ -1,17 +1,48 @@
 package com.e2eTest.automation.page_objects;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.e2eTest.automation.utils.Setup;
 
 public class LoginPage {
-	
+
+	/* Retreive Element */
+	@CacheLookup
 	@FindBy(how = How.ID, using = "Email")
 	public static WebElement email;
-	
+
+	@CacheLookup
 	@FindBy(how = How.ID, using = "Password")
 	public static WebElement password;
-	
+
+	@CacheLookup
 	@FindBy(how = How.XPATH, using = "//button[@type='submit']")
 	public static WebElement btnLogin;
+
+	public LoginPage() {
+		PageFactory.initElements(Setup.getDriver(), this);
+	}
+
+	/* Create method */
+	public void goToUrl() {
+		Setup.getDriver().get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
+	}
+
+	public void fillEmail(String mail) {
+		email.clear();
+		email.sendKeys(mail);
+	}
+
+	public void fillPassword(String psw) {
+		password.clear();
+		password.sendKeys(psw);
+	}
+
+	public void clickOnBtnLogin() {
+		btnLogin.click();
+	}
 }
--- src/test/java/com/e2eTest/automation/step_definitions/LoginStepDef.java
+++ src/test/java/com/e2eTest/automation/step_definitions/LoginStepDef.java
@@ -1,27 +1,35 @@
 package com.e2eTest.automation.step_definitions;
 
+import com.e2eTest.automation.page_objects.LoginPage;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginStepDef {
+	
+	public LoginPage loginPage;
+	
+	public LoginStepDef() {
+		loginPage = new LoginPage();
+	}
 
 	@Given("Je me connecte sur l application nopcommerce")
 	public void jeMeConnecteSurLApplicationNopcommerce() {
-	   
+		loginPage.goToUrl();
 	}
 	@When("Je saisis l Email par {string}")
-	public void jeSaisisLEmailPar(String string) {
-	   
+	public void jeSaisisLEmailPar(String email) {
+		loginPage.fillEmail(email);
 	}
 	@And("Je saisis le Password par {string}")
-	public void jeSaisisLePasswordPar(String string) {
-	 
+	public void jeSaisisLePasswordPar(String password) {
+		loginPage.fillPassword(password);
 	}
 	@And("Je clique sur le boutton LOGIN")
 	public void jeCliqueSurLeBouttonLOGIN() {
-	   
+		loginPage.clickOnBtnLogin();
 	}
 	@Then("Je me redirige vers la page Home {string}")
 	public void jeMeRedirigeVersLaPageHome(String string) {
